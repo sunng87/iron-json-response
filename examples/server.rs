b38d0ff260a25b203acd19a374a524a8666f4048
Ning Sun <sunng@about.me>
2017-03-08 16:13:36 +0800

    (feat) drop rustc_serialize and make serde default
    Signed-off-by: Ning Sun <sunng@about.me>

--- examples/server.rs
+++ examples/server.rs
@@ -1,55 +1,15 @@
 extern crate iron;
 extern crate params;
 extern crate iron_json_response as ijr;
-#[cfg(feature = "rustc_serialize_type")]
-extern crate rustc_serialize;
-#[cfg(feature = "serde_type")]
 extern crate serde;
-#[cfg(feature = "serde_type")]
 #[macro_use]
 extern crate serde_json;
-#[cfg(feature = "rustc_serialize_type")]
-#[macro_use]
-extern crate maplit;
 
 use iron::prelude::*;
 use iron::status;
 use params::{Params, Value};
 use ijr::{JsonResponse, JsonResponseMiddleware};
 
-#[cfg(feature = "rustc_serialize_type")]
-mod data {
-    use rustc_serialize::json::{ToJson, Json};
-    use std::collections::BTreeMap;
-
-    pub fn make_data() -> Json {
-        let player: Vec<BTreeMap<String, Json>> = vec![btreemap! {
-                "name".to_owned() => "Teixeira".to_owned().to_json(),
-                "apps".to_owned() => 7u16.to_json(),
-                "goals".to_owned() => 2u16.to_json(),
-                "assists".to_owned() => 1u16.to_json()
-            },
-                                                       btreemap! {
-                "name".to_owned() => "Ramires".to_owned().to_json(),
-                "apps".to_owned() => 6u16.to_json(),
-                "goals".to_owned() => 2u16.to_json(),
-                "assists".to_owned() => 3u16.to_json()
-            },
-                                                       btreemap! {
-                "name".to_owned() => "Wu Xi".to_owned().to_json(),
-                "apps".to_owned() => 7u16.to_json(),
-                "goals".to_owned() => 1u16.to_json(),
-                "assists".to_owned() => 2u16.to_json()
-            }];
-        let team = btreemap! {
-            "team".to_owned() => "Jiangsu Suning".to_owned().to_json(),
-            "players".to_owned() => player.to_json()
-        };
-        team.to_json()
-    }
-}
-
-#[cfg(feature = "serde_type")]
 mod data {
     use serde_json::value::Value;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,15 @@
 #[macro_use]
 extern crate mime;
 extern crate iron;
-#[cfg(feature = "serde_type")]
 extern crate serde;
-#[cfg(feature = "serde_type")]
 extern crate serde_json;
-#[cfg(feature = "rustc_serialize_type")]
-extern crate rustc_serialize;
 
 use iron::prelude::*;
 use iron::{AfterMiddleware, typemap};
 use iron::modifier::Modifier;
 use iron::headers::ContentType;
 
-#[cfg(feature = "rustc_serialize_type")]
-use rustc_serialize::json::{self, ToJson, Json};
-#[cfg(feature = "serde_type")]
 use serde::ser::Serialize as ToJson;
-#[cfg(feature = "serde_type")]
 use serde_json::value::{self, Value as Json};
 
 #[derive(Clone)]
@@ -27,15 +19,6 @@ pub struct JsonResponse {
 }
 
 impl JsonResponse {
-    #[cfg(feature = "rustc_serialize_type")]
-    pub fn new<T: ToJson>(value: T, callback: Option<String>) -> JsonResponse {
-        JsonResponse {
-            value: value.to_json(),
-            callback: callback,
-        }
-    }
-
-    #[cfg(feature = "serde_type")]
     pub fn new<T: ToJson>(value: T, callback: Option<String>) -> JsonResponse {
         JsonResponse {
             value: value::to_value(&value).unwrap_or(Json::Null),
@@ -67,40 +50,6 @@ impl Modifier<Response> for JsonResponse {
 }
 
 impl AfterMiddleware for JsonResponseMiddleware {
-    #[cfg(feature = "rustc_serialize_type")]
-    fn after(&self, _: &mut Request, mut resp: Response) -> IronResult<Response> {
-        let header_body =
-            resp.extensions
-                .remove::<JsonResponseMiddleware>()
-                .and_then(|j| {
-                    if let Ok(json_string) = json::encode(&j.value) {
-                        match j.callback {
-                            Some(ref cb) => {
-                                let mut jsonp = String::new();
-                                jsonp.push_str(cb);
-                                jsonp.push('(');
-                                jsonp.push_str(&json_string);
-                                jsonp.push(')');
-                                Some((ContentType(mime!(Text/Javascript; Charset=Utf8)), jsonp))
-                            }
-                            None => Some((ContentType::json(), json_string)),
-                        }
-                    } else {
-                        None
-                    }
-                });
-
-        if let Some((content_type, body)) = header_body {
-            if !resp.headers.has::<ContentType>() {
-                resp.headers.set(content_type);
-            }
-            resp.set_mut(body);
-        }
-        Ok(resp)
-    }
-
-
-    #[cfg(feature = "serde_type")]
     fn after(&self, _: &mut Request, r: Response) -> IronResult<Response> {
         let mut resp = r;
         let header_body =
