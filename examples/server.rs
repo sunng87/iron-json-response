30668b723a19a3f58e7de39e3155dbe0a4b3ea1e
Ning Sun <sunng@about.me>
2017-05-10 22:48:50 +0800

    (feat) update to serde 1.0
    Signed-off-by: Ning Sun <sunng@about.me>

--- examples/server.rs
+++ examples/server.rs
@@ -14,23 +14,25 @@ mod data {
     use serde_json::value::Value;
 
     pub fn make_data() -> Value {
-        let players = json!([{ "name": "Teixeira",
-               "apps": 7,
-               "goals": 2,
-               "assists": 1                             },
-                             { "name": "Ramires",
-               "apps": 6,
-               "goals": 2,
-               "assists": 3                             },
-                             { "name": "Wu Xi",
-               "apps": 7,
-               "goals": 1,
-            "assists": 2                             }]);
-        let team = json!({
+        json!({
             "team": "Jiangsu Suning",
-            "players": players
-        });
-        team
+            "players": [{
+                "name": "Teixeira",
+                "apps": 7,
+                "goals": 2,
+                "assists": 1
+            }, {
+                "name": "Ramires",
+                "apps": 6,
+                "goals": 2,
+                "assists": 3
+            }, {
+                "name": "Wu Xi",
+                "apps": 7,
+                "goals": 1,
+                "assists": 2
+            }]
+        })
     }
 }
 
@@ -41,14 +43,12 @@ fn handler(req: &mut Request) -> IronResult<Response> {
     let data = make_data();
 
     let cb = req.get_ref::<Params>()
-                .ok()
-                .and_then(|p| p.find(&["cb"]))
-                .and_then(|v| {
-                    match v {
-                        &Value::String(ref c) => Some(c.to_string()),
-                        _ => None,
-                    }
-                });
+        .ok()
+        .and_then(|p| p.find(&["cb"]))
+        .and_then(|v| match v {
+                      &Value::String(ref c) => Some(c.to_string()),
+                      _ => None,
+                  });
 
     let json_resp = JsonResponse::new(data, cb);
     resp.set_mut(json_resp).set_mut(status::Ok);
